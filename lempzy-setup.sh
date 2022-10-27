141784f0936ad0ef57641c03299de71393012dee
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-10-27 21:50:08 +1300

    Update Issue #9 #10 #11

--- lempzy-setup.sh
+++ lempzy-setup.sh
@@ -114,7 +114,6 @@ install_mariadb() {
      sleep 1
 }
 
-# TODO TEST SINGLE SCRIPT
 # Install PHP
 install_php() {
      if [[ "${OS_VERSION}" == "10" ]]; then
@@ -124,7 +123,16 @@ install_php() {
           echo "Debian 11 PHP"
 
      elif [[ "${OS_VERSION}" == "18.04" ]]; then
-          echo "Ubuntu 18.04 PHP"
+          apt-get install software-properties-common
+          add-apt-repository -y ppa:ondrej/php
+          apt update
+          apt install php7.3-fpm php-mysql -y
+          apt install php7.3-common php7.3-zip php7.3-curl php7.3-xml php7.3-xmlrpc php7.3-json php7.3-mysql php7.3-pdo php7.3-gd php7.3-imagick php7.3-ldap php7.3-imap php7.3-mbstring php7.3-intl php7.3-cli php7.3-recode php7.3-tidy php7.3-bcmath php7.3-opcache -y
+          apt-get purge php8.* -y
+          apt-get autoclean
+          apt-get autoremove
+          echo ""
+          sleep 1
 
      elif [[ "${OS_VERSION}" == "20.04" ]]; then
           echo "${grn}Installing PHP ...${end}"
@@ -158,17 +166,36 @@ install_nginx() {
      sleep 1
 }
 
-# TODO TEST SINGLE SCRIPT
 # Configure PHP FPM
 configure_php_fpm() {
      # Get PHP Installed Version
      PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
 
      if [[ "${PHP_VERSION}" == "7.2" ]]; then
-          echo "Debian 10 PHP"
+          echo "${grn}Configuring PHP FPM ...${end}"
+          echo ""
+          sleep 3
+          sed -i "s/max_execution_time = 30/max_execution_time = 360/g" /etc/php/7.2/fpm/php.ini
+          sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/7.2/fpm/php.ini
+          sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/7.2/fpm/php.ini
+          sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/fpm/php.ini
+          sed -i "s/upload_max_filesize = .*/upload_max_filesize = 256M/" /etc/php/7.2/fpm/php.ini
+          sed -i "s/post_max_size = .*/post_max_size = 256M/" /etc/php/7.2/fpm/php.ini
+          echo ""
+          sleep 1
 
      elif [[ "${PHP_VERSION}" == "7.3" ]]; then
-          echo "Debian 11 PHP"
+          echo "${grn}Configuring PHP FPM ...${end}"
+          echo ""
+          sleep 3
+          sed -i "s/max_execution_time = 30/max_execution_time = 360/g" /etc/php/7.3/fpm/php.ini
+          sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/7.3/fpm/php.ini
+          sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/7.3/fpm/php.ini
+          sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/fpm/php.ini
+          sed -i "s/upload_max_filesize = .*/upload_max_filesize = 256M/" /etc/php/7.3/fpm/php.ini
+          sed -i "s/post_max_size = .*/post_max_size = 256M/" /etc/php/7.3/fpm/php.ini
+          echo ""
+          sleep 1
 
      elif [[ "${PHP_VERSION}" == "7.4" ]]; then
           echo "${grn}Configuring PHP FPM ...${end}"
@@ -202,7 +229,6 @@ install_memcached() {
      sleep 1
 }
 
-# TODO TEST SINGLE SCRIPT
 # Restart Services
 restart_services() {
      # Get PHP Installed Version
@@ -224,7 +250,6 @@ restart_services() {
      systemctl restart nginx
 }
 
-# TODO TEST SINGLE SCRIPT
 # Installing IONCUBE
 install_ioncube() {
      echo "${grn}Installing IONCUBE ...${end}"
@@ -237,23 +262,32 @@ install_ioncube() {
      # PHP Version
      PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
 
-
-     if [[ "${PHP_VERSION}" == "7.2" ]]; then
-          systemctl restart php7.2-fpm.service
-
-     elif [[ "${PHP_VERSION}" == "7.3" ]]; then
-          systemctl restart php7.3-fpm.service
-
-     elif [[ "${PHP_VERSION}" == "7.4" ]]; then
-          # Download ioncube
+     # Download ioncube
      wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
      tar -xvzf ioncube_loaders_lin_x86-64.tar.gz
      rm -f ioncube_loaders_lin_x86-64.tar.gz
 
      # Copy files to modules folder
      sudo cp "ioncube/ioncube_loader_lin_${PHP_VERSION}.so" $MODULES
-     echo "zend_extension=$MODULES/ioncube_loader_lin_${PHP_VERSION}.so" >>/etc/php/7.4/fpm/php.ini
-     echo "zend_extension=$MODULES/ioncube_loader_lin_${PHP_VERSION}.so" >>/etc/php/7.4/cli/php.ini
+
+     if [[ "${PHP_VERSION}" == "7.2" ]]; then
+          # Copy files to modules folder
+          echo "zend_extension=$MODULES/ioncube_loader_lin_${PHP_VERSION}.so" >>/etc/php/7.2/fpm/php.ini
+          echo "zend_extension=$MODULES/ioncube_loader_lin_${PHP_VERSION}.so" >>/etc/php/7.2/cli/php.ini
+
+     elif [[ "${PHP_VERSION}" == "7.3" ]]; then
+          # Copy files to modules folder
+          echo "zend_extension=$MODULES/ioncube_loader_lin_${PHP_VERSION}.so" >>/etc/php/7.3/fpm/php.ini
+          echo "zend_extension=$MODULES/ioncube_loader_lin_${PHP_VERSION}.so" >>/etc/php/7.3/cli/php.ini
+
+     elif [[ "${PHP_VERSION}" == "7.4" ]]; then
+          # Copy files to modules folder
+          echo "zend_extension=$MODULES/ioncube_loader_lin_${PHP_VERSION}.so" >>/etc/php/7.4/fpm/php.ini
+          echo "zend_extension=$MODULES/ioncube_loader_lin_${PHP_VERSION}.so" >>/etc/php/7.4/cli/php.ini
+     else
+          echo -e "${red}Sorry, This script is designed for DEBIAN (10, 11), UBUNTU (18.04, 20.04, 22.04, 22.10)${end}"
+          exit 1
+     fi
 
      rm -rf ioncube
 
@@ -261,15 +295,13 @@ install_ioncube() {
 
      echo ""
      sleep 1
-     else
-          echo -e "${red}Sorry, This script is designed for DEBIAN (10, 11), UBUNTU (18.04, 20.04, 22.04, 22.10)${end}"
-          exit 1
-     fi
 }
 
-# TODO TEST SINGLE SCRIPT
 # Install Mcrypt
 install_mcrpyt() {
+     # PHP Modules folder
+     MODULES=$(php -i | grep ^extension_dir | awk '{print $NF}')
+
      # Get PHP Installed Version
      PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
 
@@ -277,7 +309,15 @@ install_mcrpyt() {
           systemctl restart php7.2-fpm.service
 
      elif [[ "${PHP_VERSION}" == "7.3" ]]; then
-          systemctl restart php7.3-fpm.service
+          apt-get install php7.3-dev
+          apt-get -y install gcc make autoconf libc-dev pkg-config
+          apt-get -y install libmcrypt-dev
+          yes | pecl install mcrypt-1.0.3
+          echo "extension=$MODULES/mcrypt.so" >>/etc/php/7.3/fpm/php.ini
+          echo "extension=$MODULES/mcrypt.so" >>/etc/php/7.3/cli/php.ini
+          restart_services
+          echo ""
+          sleep 1
 
      elif [[ "${PHP_VERSION}" == "7.4" ]]; then
           echo "${grn}Installing Mcrypt ...${end}"
