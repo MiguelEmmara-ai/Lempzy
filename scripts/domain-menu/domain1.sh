f6f7e3b8559734339198ec766c8109b6885a0329
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-10-28 21:21:55 +1300

    Update Sed -i

--- scripts/domain-menu/domain1.sh
+++ scripts/domain-menu/domain1.sh
@@ -127,7 +127,7 @@ sed -i "s/phpX.X/php$PHP_VERSION/g" $sitesAvailable$configName
 php7_dotdeb="https://raw.githubusercontent.com/MiguelEmmara-ai/Lempzy/development/scripts/php7dotdeb"
 wget -q $php7_dotdeb -O /etc/php/$PHP_VERSION/fpm/pool.d/$domain.conf
 sed -i "s/domain.com/$domain/g" /etc/php/$PHP_VERSION/fpm/pool.d/$domain.conf
-sed -i "s/phpX.X-a.com-fpm.sock/php$PHP_VERSION-$domain-fpm.sock/g" /etc/php/$PHP_VERSION/fpm/pool.d/$domain.conf
+sed -i "s/phpX.X/php$PHP_VERSION/g" /etc/php/$PHP_VERSION/fpm/pool.d/$domain.conf
 echo "" >>/etc/php/$PHP_VERSION/fpm/pool.d/$domain.conf
 dos2unix /etc/php/$PHP_VERSION/fpm/pool.d/$domain.conf >/dev/null 2>&1
 service php$PHP_VERSION-fpm reload
