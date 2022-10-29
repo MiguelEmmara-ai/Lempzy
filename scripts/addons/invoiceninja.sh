803ff4c33ad7ba3f0de910ca7022d67575fe9d9a
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-10-30 11:49:45 +1300

    Fix https://github.com/MiguelEmmara-ai/Lempzy/issues/26

--- scripts/addons/invoiceninja.sh
+++ scripts/addons/invoiceninja.sh
@@ -16,8 +16,8 @@ end=$'\e[0m'
 
 # Check if you are root
 if [ "$(whoami)" != 'root' ]; then
-    echo "You have no permission to run $0 as non-root user. Use sudo"
-    exit 1
+     echo "You have no permission to run $0 as non-root user. Use sudo"
+     exit 1
 fi
 
 # Variables
@@ -32,100 +32,129 @@ PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
 
 # Ask the user to add domain name
 while true; do
-    clear
-    clear
-    echo "########################### SERVER CONFIGURED BY MIGUEL EMMARA ###########################"
-    echo "                                   ${grn}INSTALL INVOICE NINJA${end}"
-    echo ""
-    echo "     __                                    "
-    echo "    / /   ___  ____ ___  ____  ____  __  __"
-    echo "   / /   / _ \/ __ \`__ \/ __ \/_  / / / / /"
-    echo "  / /___/  __/ / / / / / /_/ / / /_/ /_/ /"
-    echo " /_____/\___/_/ /_/ /_/ .___/ /___/\__, /"
-    echo "                   /_/          /____/_/"
-    echo ""
-    echo "${grn}Press [CTRL + C] to cancel...${end}"
+     clear
+     clear
+     echo "########################### SERVER CONFIGURED BY MIGUEL EMMARA ###########################"
+     echo "                                   ${grn}INSTALL INVOICE NINJA${end}"
+     echo ""
+     echo "     __                                    "
+     echo "    / /   ___  ____ ___  ____  ____  __  __"
+     echo "   / /   / _ \/ __ \`__ \/ __ \/_  / / / / /"
+     echo "  / /___/  __/ / / / / / /_/ / / /_/ /_/ /"
+     echo " /_____/\___/_/ /_/ /_/ .___/ /___/\__, /"
+     echo "                   /_/          /____/_/"
+     echo ""
+     echo "${grn}Press [CTRL + C] to cancel...${end}"
 
-    echo "Note* this will erase all of your data on your domain folder, then install Invoice Ninja!"
-    echo "Preferably install Invoice Ninja on your subdomain [eg, invoiceninja.domain.com]"
-    echo "Feel free to backup any important files before hand!"
-    echo ""
-    echo "Here all the domain on you server"
-    echo ""
-    echo "_____________"
-    echo "${blu}"
-    ls -I default -I phpmyadmin -I filemanager -1 /etc/nginx/sites-enabled/
-    echo "${end}_____________"
-    echo ""
-    read -p ${grn}"Please provide domain [eg, manage.domain.com]${end}: " domain
-    read -p ${grn}"Please type your domain one more time${end}: " domain2
-    echo
-    [ "$domain" = "$domain2" ] && break
-    echo "Domain you provide does not match, please try again!"
-    read -p "${grn}Press [Enter] key to continue...${end}" readEnterKey
+     echo "Note* this will erase all of your data on your domain folder, then install Invoice Ninja!"
+     echo "Preferably install Invoice Ninja on your subdomain [eg, invoiceninja.domain.com]"
+     echo "Feel free to backup any important files before hand!"
+     echo ""
+     echo "Here all the domain on you server"
+     echo ""
+     echo "_____________"
+     echo "${blu}"
+     ls -I default -I phpmyadmin -I filemanager -1 /etc/nginx/sites-enabled/
+     echo "${end}_____________"
+     echo ""
+     read -p ${grn}"Please provide domain [eg, manage.domain.com]${end}: " domain
+     read -p ${grn}"Please type your domain one more time${end}: " domain2
+     echo
+     [ "$domain" = "$domain2" ] && break
+     echo "Domain you provide does not match, please try again!"
+     read -p "${grn}Press [Enter] key to continue...${end}" readEnterKey
 done
 
 until [[ $domain =~ $domainRegex ]]; do
-    echo -n "Enter valid domain: "
-    read domain
+     echo -n "Enter valid domain: "
+     read domain
 done
 
 # Check if domain is not there
 FILE=/etc/nginx/sites-available/$domain
 file2=/var/www/$domain
 if [ -f "$FILE" ] || [ -f "$file2" ]; then
-    clear
+     clear
 else
-    echo ""
-    echo "$domain does not exist, please install your domain and try again"
-    exit
+     echo ""
+     echo "$domain does not exist, please install your domain and try again"
+     exit
 fi
 
 # Install InvoiceNinja
-rm -rf /var/www/$domain/*
-cd /var/www/$domain/
-wget https://github.com/invoiceninja/invoiceninja/releases/download/v5.1.14-release/invoiceninja.zip
-unzip invoiceninja.zip
-chown -R www-data:www-data /var/www/$domain
-chown -R www-data:www-data /var/www/$domain
-sudo chmod -R g+s /var/www/$domain
-chown www-data:www-data /var/www/
-chown -R $USER:$USER /var/www/$domain       # JUST TO MAKE SURE
-chown -R www-data:www-data /var/www/$domain # JUST TO MAKE SURE
-
-# Installing and configuring Invoice Ninja server software and dependencies
-yes | sudo php /usr/local/bin/composer install --no-dev
+install_invoice_ninja() {
+     rm -rf /var/www/$domain/*
+     cd /var/www/$domain/
+     wget https://github.com/invoiceninja/invoiceninja/releases/download/v5.5.35/invoiceninja.zip
+     unzip invoiceninja.zip
+     chown -R www-data:www-data /var/www/$domain
+     chown -R www-data:www-data /var/www/$domain
+     sudo chmod -R g+s /var/www/$domain
+     chown www-data:www-data /var/www/
+     chown -R $USER:$USER /var/www/$domain       # JUST TO MAKE SURE
+     chown -R www-data:www-data /var/www/$domain # JUST TO MAKE SURE
+}
 
 # Change vhost to no fastcgi cache made for InvoiceNinja
-configName=$domain
-cd $sitesAvailable
-cp /root/Lempzy/scripts/vhost-nocache $sitesAvailable$domain
-sed -i "s/domain.com/$domain/g" $sitesAvailable$configName
+vhost_switch() {
+     configName=$domain
+     cd $sitesAvailable
+     cp /root/Lempzy/scripts/vhost-nocache $sitesAvailable$domain
+     sed -i "s/domain.com/$domain/g" $sitesAvailable$configName
+}
+
 # Create NEW Database For Invoice Ninja
-#domainClear=${domain//./} # Domain name variable
-#domainClear2=${domainClear//-/} # Domain name variable
-#password_invoiceNinja=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1` # Generate random password and save it to password_invoiceNinja variable.
-#mysql -uroot <<MYSQL_SCRIPT
-#CREATE DATABASE invoice_db_$domainClear2;
-#CREATE USER 'invoice_usr_$domainClear2'@'localhost' IDENTIFIED BY '$password_invoiceNinja';
-#GRANT ALL PRIVILEGES ON invoice_db_$domainClear2.* TO 'invoice_usr_$domainClear2'@'localhost';
-#FLUSH PRIVILEGES;
-#MYSQL_SCRIPT
+create_new_db_invoice() {
+     domainClear=${domain//./}
+     domainClear2=${domainClear//-/}
 
-#ninjaDatabase='ninja'
-#ninjaUser='ninja'
-#ninjaPassword='ninja'
-#mysql -uroot <<MYSQL_SCRIPT
-#CREATE DATABASE $ninjaDatabase;
-#CREATE USER '$ninjaUser'@'localhost' IDENTIFIED BY '$ninjaPassword';
-#GRANT ALL PRIVILEGES ON $DB.* TO '$ninjaUser'@'localhost';
-#FLUSH PRIVILEGES;
-#MYSQL_SCRIPT
+     # Generate random password and save it to password_invoiceNinja variable.
+     password_invoiceNinja=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 
-# Optimize artisan
-cd /var/www/$domain
-php artisan optimize
-cd
+     mysql -uroot <<MYSQL_SCRIPT
+     CREATE DATABASE invoice_db_$domainClear2;
+     CREATE USER 'invoice_usr_$domainClear2'@'localhost' IDENTIFIED BY '$password_invoiceNinja';
+     GRANT ALL PRIVILEGES ON invoice_db_$domainClear2.* TO 'invoice_usr_$domainClear2'@'localhost';
+     FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+}
+
+# Configure InvoiceNinja
+configure_invoice_ninja() {
+     cd /var/www/$domain/
+     cp .env.example .env
+     sed -i "s/DB_DATABASE=ninja/DB_DATABASE=invoice_db_$domainClear2/g" .env
+     sed -i "s/DB_USERNAME=ninja/DB_USERNAME=invoice_usr_$domainClear2/g" .env
+     sed -i "s/DB_PASSWORD=ninja/DB_PASSWORD=password_invoiceNinja/g" .env
+
+     # Optimize artisan
+     cd /var/www/$domain
+
+     # Create and override .env file's APP_KEY value (php artisan key:generate)
+     while IFS="" read -r p || [ -n "$p" ]; do
+          if printf '%s' "$p" | grep -Eq '^APP_KEY'; then
+               key=$(php artisan key:generate --show)
+               echo "APP_KEY=$key" >>.envv
+               printf '%s\n' "APP_KEY generated and set"
+          else
+               #printf '%s\n' "$p"
+               echo "$p" >>.envv
+          fi
+
+     done <.env
+
+     cp .envv .env
+     rm .envv
+
+     php artisan optimize
+     cd
+}
+
+# Run
+install_invoice_ninja
+vhost_switch
+create_new_db_invoice
+configure_invoice_ninja
 
 # Restart nginx and php-fpm
 echo "Restart Nginx & PHP-FPM ..."
@@ -149,10 +178,6 @@ echo ""
 echo "Complete! $domain has been installed with Invoice Ninja!"
 echo "Navigate to ${grn}$domain${end} in your browser to configure Invoice Ninja"
 echo ""
-#echo "Database Name: invoice_db_$domainClear2"
-#echo "User Name: invoice_usr_$domainClear2"
-#echo "Password: $password_invoiceNinja"
-
-#echo "Database Name: $ninjaDatabase"
-#echo "User Name: $ninjaUser"
-#echo "Password: $ninjaPassword"
+echo "Database Name: invoice_db_$domainClear2"
+echo "User Name: invoice_usr_$domainClear2"
+echo "Password: $password_invoiceNinja"
